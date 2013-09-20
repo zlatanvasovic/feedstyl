7776ae77c1a1a51c95edda2b5a52be9f0568adaf
Zlatan Vasović <legospace9876@gmail.com>
2013-09-20 16:43:21 +0200

    Use data directly, without var

--- parser.py
+++ parser.py
@@ -11,9 +11,6 @@ import feedparser
 # Set up
 # ------
 
-# Data for parse
-data = feedparser.parse(sys.argv[1])
-
 # List of uples (label, property-tag, truncation)
 feed_properties = [
   ("\n\033[1mFeed title:\033[0m", "title", None),
@@ -33,14 +30,14 @@ item_properties = [
 if __name__ == "__main__":
   # Display feed data
   for label, prop, trunc in feed_properties:
-    value = data.feed[prop]
+    value = feedparser.parse(sys.argv[1]).feed[prop]
     if trunc:
       value = value[:trunc] + "..."
     print >> sys.stdout, label, value
 
   # Display item data
   print >> sys.stdout, "\n\033[1mFeed items:\033[0m\n"
-  for item in data.entries:
+  for item in feedparser.parse(sys.argv[1]).entries:
     for label, prop, trunc in item_properties:
       value = item[prop]
       if trunc:
