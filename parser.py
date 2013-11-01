351c674f5483959134de48aab42e72fc376455d2
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-11-01 20:56:44 +0100

    New description variable names

--- parser.py
+++ parser.py
@@ -14,10 +14,10 @@ data = feedparser.parse(argv[1])
 # Display core feed data
 print "\n\033[1mFeed title:\033[0m", data.feed.title
 if "description" in data.feed:
-  feed_d = data.feed.description
-  if len(feed_d) > 59:
-    feed_d = feed_d[:59] + "..."
-  print "\033[1mFeed description:\033[0m", feed_d
+  df = data.feed.description
+  if len(df) > 59:
+    df = df[:59] + "..."
+  print "\033[1mFeed description:\033[0m", df
 print "\033[1mFeed URL:\033[0m", data.feed.link
 
 # Display core items data
@@ -25,8 +25,8 @@ print "\n\033[1mFeed items:\033[0m\n"
 for item in data.entries:
   print "  \033[1mItem title:\033[0m", item.title
   if "description" in item:
-    item_d = item.description
-    if len(item_d) > 55:
-      item_d = item_d[:55] + "..."
-    print "  \033[1mItem description:\033[0m", item_d
+    di = item.description
+    if len(di) > 55:
+      di = di[:55] + "..."
+    print "  \033[1mItem description:\033[0m", di
   print "  \033[1mItem link:\033[0m", item.link, "\n"
