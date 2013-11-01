815b3f740489d2b68b7e2532aed740ab6ec7c16b
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-11-01 20:58:26 +0100

    Remove variables

--- parser.py
+++ parser.py
@@ -14,10 +14,9 @@ data = feedparser.parse(argv[1])
 # Display core feed data
 print "\n\033[1mFeed title:\033[0m", data.feed.title
 if "description" in data.feed:
-  df = data.feed.description
-  if len(df) > 59:
-    df = df[:59] + "..."
-  print "\033[1mFeed description:\033[0m", df
+  if len(data.feed.description) > 59:
+    data.feed.description = data.feed.description[:59] + "..."
+  print "\033[1mFeed description:\033[0m", data.feed.description
 print "\033[1mFeed URL:\033[0m", data.feed.link
 
 # Display core items data
@@ -25,8 +24,7 @@ print "\n\033[1mFeed items:\033[0m\n"
 for item in data.entries:
   print "  \033[1mItem title:\033[0m", item.title
   if "description" in item:
-    di = item.description
-    if len(di) > 55:
-      di = di[:55] + "..."
-    print "  \033[1mItem description:\033[0m", di
+    if len(item.description) > 55:
+      item.description = item.description[:55] + "..."
+    print "  \033[1mItem description:\033[0m", item.description
   print "  \033[1mItem link:\033[0m", item.link, "\n"
